1e1a73ac44467d7ca5b48d56107d5dd2ac3aa6e8
Kim Reuter <kimbaerlyyo@web.de>
2025-05-07 09:56:21 +0200

    fix: remove deprecated appDir from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true, // stellt sicher, dass App Router aktiv ist
-    },
-    output: 'standalone', // wichtig für saubere Vercel Deployments
-  };
-  
-  module.exports = nextConfig;
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
